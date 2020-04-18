8c05e03fe716bac57cb327826c0999a7010e51d9
Angelo Teixeira <imnotteixeira@gmail.com>
2020-04-18 23:26:02 +0100

    Fixed max steps verification

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 require("dotenv-flow").config();
 
 module.exports = {
-    POPULATION_SIZE: process.env.POPULATION_SIZE,
-    MATRIX_SIDE: Math.sqrt(process.env.POPULATION_SIZE),
-    SPREAD_RADIUS: process.env.SPREAD_RADIUS,
-    MAX_STEPS: process.env.MAX_STEPS,
-    HYGIENE_DISREGARD: process.env.HYGIENE_DISREGARD,
+    POPULATION_SIZE: parseInt(process.env.POPULATION_SIZE, 10),
+    MATRIX_SIDE: Math.sqrt(parseInt(process.env.POPULATION_SIZE, 10)),
+    SPREAD_RADIUS: parseInt(process.env.SPREAD_RADIUS, 10),
+    MAX_STEPS: parseInt(process.env.MAX_STEPS, 10),
+    HYGIENE_DISREGARD: parseFloat(process.env.HYGIENE_DISREGARD),
 };
--- src/simulation.js
+++ src/simulation.js
@@ -10,25 +10,26 @@ const simulate = (simulation, maxSteps) => {
         hygieneDisregard,
     } = simulation;
 
+    console.info(`Simulating${maxSteps ? ` with max steps = ${maxSteps}` : ""}...`);
+
     let step = 0;
-    if (population.length <= 100) {
-        displayMatrix(population);
-    } else {
-        console.info("Population too big to render");
+    if (population.length > 100) {
+        console.info("Population too big. Will not render population.");
     }
     MetricsService.collect("carrier-count", { carriers });
-    while ((maxSteps && step < maxSteps) || (!maxSteps && !population.every((_, i) => isContaminated(population, i)))) {
+    while (!population.every((_, i) => isContaminated(population, i))) {
 
+        // console.debug("Step: ", step);
 
         step++;
         propagateDisease(population, carriers, spreadRadius, hygieneDisregard);
         if (population.length <= 100) {
             displayMatrix(population);
-        } else {
-            console.info("Population too big to render");
         }
 
         MetricsService.collect("carrier-count", { carriers });
+
+        if (maxSteps && step === maxSteps) break;
 
     }
 };
