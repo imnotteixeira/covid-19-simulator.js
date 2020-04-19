79f6120044ac29e5b31075a3a6c79ec41f3f89a9
Angelo Teixeira <imnotteixeira@gmail.com>
2020-04-19 23:15:36 +0100

    Added demo susceptibility distribution

--- src/init.js
+++ src/init.js
@@ -2,17 +2,36 @@ const { IndividualStates } = require("./utils");
 const config = require("./config");
 const { contaminate } = require("./disease/transmission");
 
+/**
+ * Generates size values of S, according to distribution
+ * @param {*} size Number of people
+ */
+const generateSusceptibilityDistribution = (size) => {
+
+    // Hardcoding distribution, change later
+    const distribution = (x) => Math.exp(-Math.pow((x - (size / 2)), 2) / size);
+
+    return Array(size).fill(0).map((_, i) => distribution(i));
+
+};
+
 const createIndividual = (susceptibility) => ({
     susceptibility,
     daysSinceTransmission: -1,
     state: IndividualStates.HEALTHY,
 });
+
 const initPopulation = (size) => {
     if (size > 0 && Math.sqrt(size) % 1 === 0) {
         const population = [];
+
+        const susceptibilities = generateSusceptibilityDistribution(size);
+
         for (let i = 0; i < size; i++) {
-            population[i] = createIndividual(1);
+            population[i] = createIndividual(susceptibilities[i]);
         }
+
+        console.log(population);
         return population;
 
     } else {
